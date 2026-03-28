fd1c24bd585b01edfebcc387b0e72c536df44429
FeldymZ <feldy.moundzegou@gmail.com>
2026-03-28 14:58:43 +0100

    Description de la mise à jour

--- src/main/java/com/school/api/formation/continues/controller/publics/FormationContinuesPublicController.java
+++ src/main/java/com/school/api/formation/continues/controller/publics/FormationContinuesPublicController.java
@@ -1,6 +1,6 @@
 package com.school.api.formation.continues.controller.publics;
 
-import com.school.api.formation.continues.entity.FormationContinues;
+import com.school.api.formation.continues.dto.FormationDTO;
 import com.school.api.formation.continues.service.FormationContinuesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -13,16 +13,20 @@ public class FormationContinuesPublicController {
 
     private final FormationContinuesService service;
 
+    /* ================= GET ALL ================= */
+
     @GetMapping
-    public Page<FormationContinues> getAll(
+    public Page<FormationDTO> getAll(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
         return service.getAllPublic(page, size);
     }
 
+    /* ================= GET BY ID ================= */
+
     @GetMapping("/{id}")
-    public FormationContinues getById(@PathVariable Long id) {
+    public FormationDTO getById(@PathVariable Long id) {
         return service.getById(id);
     }
 }
